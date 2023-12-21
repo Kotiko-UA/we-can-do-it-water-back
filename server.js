2959a0a9ae0c5bf5257953d0c43790867ef6229a
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-12-21 17:51:11 +0200

    1

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
-import app from './app.js';
+// import app from './app.js';
 const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3030, () => {
-      console.log('Database connection successful');
-    });
+    // app.listen(3030, () => {
+    //   console.log('Database connection successful');
+    // });
   })
   .catch((error) => {
     console.log(error.message);
