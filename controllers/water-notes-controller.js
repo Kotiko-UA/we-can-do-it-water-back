591151764e2f9861a5f5f5bd361b12b0237314c6
lusya0409 <lusya0409@gmail.com>
2023-12-22 21:20:52 +0200

    changes water-notes endpoints

--- controllers/water-notes-controller.js
+++ controllers/water-notes-controller.js
@@ -1,18 +1,37 @@
-// import { waterNotes } from "../models/index.js";
 import WaterNote from "../models/WaterNote.js";
 import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
+const daysInMonth = (y, mon) => {
+  let month = Number(mon);
+  let year = Number(y);
+  let nextMonth = month + 1;
+  let nextYear = year;
+  if (month + 1 === 13) {
+    nextMonth = 1;
+    nextYear = year + 1;
+  }
+  const date1 = new Date(year, month - 1, 1);
+  const date2 = new Date(nextYear, nextMonth - 1, 1);
+  return Math.round((date2 - date1) / 1000 / 3600 / 24);
+};
+
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10, ...filterParams } = req.query;
-  const skip = (page - 1) * limit;
+  const date = new Date();
+  const {
+    month = date.getUTCMonth() + 1,
+    year = date.getUTCFullYear(),
+    ...filterParams
+  } = req.query;
   const filter = { owner, ...filterParams };
-
-  const result = await WaterNote.find(filter, "name email", {
-    skip,
-    limit,
-  }).populate("owner", "email");
+  const result = await WaterNote.find({
+    owner,
+    createdAt: {
+      $gte: new Date(year, month - 1, 1),
+      $lt: new Date(year, month - 1, daysInMonth(year, month), 23, 59, 59),
+    },
+  });
   res.json(result);
 };
 
@@ -20,7 +39,6 @@ const getById = async (req, res) => {
   const { id } = req.params;
   const { _id: owner } = req.user;
 
-  // const result = await WaterNote.findOne({ _id: id });
   const result = await WaterNote.findOne({ _id: id, owner });
 
   if (!result) {
